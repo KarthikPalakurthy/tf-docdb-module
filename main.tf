545fcb61d511d74ce7829abff66ccc3d62498970
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-05-09 12:58:45 +0100

    terraform

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_docdb_subnet_group" "default" {
   )
 }
 
-resource "aws_security_group" "allow_tls" {
+resource "aws_security_group" "docdb" {
   name        = "${var.env}-docdb-security-group"
   description = "${var.env}-docdb-security-group"
   vpc_id      = var.vpc_id
@@ -34,10 +34,21 @@ resource "aws_security_group" "allow_tls" {
   )
 }
 
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "${var.env}-docdb-cluster"
+  engine                  = "docdb"
+  version                 = var.engine_version
+  master_username         = data.DB_ADMIN_USER
+  master_password         = data.DB_ADMIN_PASS
+  skip_final_snapshot     = true
+  db_subnet_group_name = aws_docdb_subnet_group.default.name
+  vpc_security_group_ids = [aws_security_group.docdb.id]
+}
+
 resource "aws_docdb_cluster_instance" "cluster_instances" {
   count              = var.no_of_instances
   identifier         = "${var.env}docdb-cluster-${count.index+1}"
   cluster_identifier = aws_docdb_cluster.docdb.id
   instance_class     = var.instance_class
-  engine_version = var.engine_version
+  engine_version     = var.engine_version
 }
